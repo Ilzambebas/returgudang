b6faf0b7d4b944f0251f09f99f278a0391f4a31d
Rifjan Jundila <rjndla@gmail.com>
2023-08-01 23:27:03 +0700

    wqtqwt

--- resources/views/admin/master/user/tambah.blade.php
+++ resources/views/admin/master/user/tambah.blade.php
@@ -25,55 +25,55 @@
                         @csrf
                         <div class="p-0 overflow-x-scroll overflow-x-auto mt-4">
                             <div class="flex flex-wrap -mx-2">
-                                <div class="p-6 space-y-6">
-                                    <div>
-                                        <label class="block text-sm">
-                                            Nama User
-                                        </label>
-                                        <input type="text" class="placeholder:text-gray-500 text-sm focus:shadow-primary-outline leading-5.6 ease block w-full appearance-none rounded-lg border border-solid border-gray-300 bg-white bg-clip-padding py-2 px-3 font-normal text-gray-700 transition-all focus:border-blue-500 focus:bg-white focus:text-gray-700 focus:outline-none focus:transition-shadow" name="nama_user" placeholder="Nama Lengkap" required/>
-                                    </div>
-                                    <div>
-                                        <label class="block text-sm">
-                                            Username
-                                        </label>
-                                        <input type="text" class="placeholder:text-gray-500 text-sm focus:shadow-primary-outline leading-5.6 ease block w-full appearance-none rounded-lg border border-solid border-gray-300 bg-white bg-clip-padding py-2 px-3 font-normal text-gray-700 transition-all focus:border-blue-500 focus:bg-white focus:text-gray-700 focus:outline-none focus:transition-shadow" name="username" placeholder="Username"/>
-                                    </div>
-                                    <div>
-                                        <label class="block text-sm">
-                                            Password
-                                        </label>
-                                        <div class="relative w-full">
-                                            <div class="absolute inset-y-0 right-0 flex items-center px-2">
-                                              <input class="hidden js-password-toggle" id="toggle" type="checkbox" />
-                                              <label class="bg-gray-300 hover:bg-gray-400 rounded px-2 py-1 text-sm text-gray-600 font-mono cursor-pointer js-password-label" for="toggle">show</label>
-                                            </div>
-                                            <input id="password" class="
-                                                            block mt-1 w-full js-password
-                                                            placeholder:text-gray-500 text-sm focus:shadow-primary-outline leading-5.6 ease block w-full appearance-none rounded-lg border border-solid border-gray-300 bg-white bg-clip-padding py-2 px-3 font-normal text-gray-700 transition-all focus:border-blue-500 focus:bg-white focus:text-gray-700 focus:outline-none focus:transition-shadow"
-                                                            type="password"
-                                                            name="password"
-                                                            placeholder="Masukkan password"
-                                                            required autocomplete="current-password" />
+                                 <div class="p-6 space-y-6">
+                                <div class="w-full md:w-1/2 px-2 mb-4">
+                                    <label class="block text-sm">
+                                        Nama User
+                                    </label>
+                                    <input type="text" class="placeholder:text-gray-500 text-sm focus:shadow-primary-outline leading-5.6 ease block w-full appearance-none rounded-lg border border-solid border-gray-300 bg-white bg-clip-padding py-2 px-3 font-normal text-gray-700 transition-all focus:border-blue-500 focus:bg-white focus:text-gray-700 focus:outline-none focus:transition-shadow" name="nama_user" placeholder="Nama Lengkap" required/>
+                                </div>
+                                <div class="w-full md:w-1/2 px-2 mb-4">
+                                    <label class="block text-sm">
+                                        Username
+                                    </label>
+                                    <input type="text" class="placeholder:text-gray-500 text-sm focus:shadow-primary-outline leading-5.6 ease block w-full appearance-none rounded-lg border border-solid border-gray-300 bg-white bg-clip-padding py-2 px-3 font-normal text-gray-700 transition-all focus:border-blue-500 focus:bg-white focus:text-gray-700 focus:outline-none focus:transition-shadow" name="username" placeholder="Username"/>
+                                </div>
+                                <div class="w-full md:w-1/2 px-2 mb-4">
+                                    <label class="block text-sm">
+                                        Password
+                                    </label>
+                                    <div class="relative w-full">
+                                        <div class="absolute inset-y-0 right-0 flex items-center px-2">
+                                        <input class="hidden js-password-toggle" id="toggle" type="checkbox" />
+                                        <label class="bg-gray-300 hover:bg-gray-400 rounded px-2 py-1 text-sm text-gray-600 font-mono cursor-pointer js-password-label" for="toggle">show</label>
                                         </div>
-                                    </div>
-                                    <div>
-                                        <label class="block text-sm">
-                                            No HP
-                                        </label>
-                                        <input type="text" name="no_hp" placeholder="No HP" class="placeholder:text-gray-500 text-sm focus:shadow-primary-outline leading-5.6 ease block w-full appearance-none rounded-lg border border-solid border-gray-300 bg-white bg-clip-padding py-2 px-3 font-normal text-gray-700 transition-all focus:border-blue-500 focus:bg-white focus:text-gray-700 focus:outline-none focus:transition-shadow" />
-                                    </div>
-
-                                    <div>
-                                        <label class="block text-sm">
-                                            Level
-                                        </label>
-                                        <select name="level"  id="level" required class="border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" >
-                                            <option value="0" hidden="">-- Pilih Disini --</option>
-                                            <option value="admin">Admin</option>
-                                            <option value="user">User</option>
-                                        </select>
+                                        <input id="password" class="
+                                                        block mt-1 w-full js-password
+                                                        placeholder:text-gray-500 text-sm focus:shadow-primary-outline leading-5.6 ease block w-full appearance-none rounded-lg border border-solid border-gray-300 bg-white bg-clip-padding py-2 px-3 font-normal text-gray-700 transition-all focus:border-blue-500 focus:bg-white focus:text-gray-700 focus:outline-none focus:transition-shadow"
+                                                        type="password"
+                                                        name="password"
+                                                        placeholder="Masukkan password"
+                                                        required autocomplete="current-password" />
                                     </div>
                                 </div>
+                                <div class="w-full md:w-1/2 px-2 mb-4">
+                                    <label class="block text-sm">
+                                        No HP
+                                    </label>
+                                    <input type="text" name="no_hp" placeholder="No HP" class="placeholder:text-gray-500 text-sm focus:shadow-primary-outline leading-5.6 ease block w-full appearance-none rounded-lg border border-solid border-gray-300 bg-white bg-clip-padding py-2 px-3 font-normal text-gray-700 transition-all focus:border-blue-500 focus:bg-white focus:text-gray-700 focus:outline-none focus:transition-shadow" />
+                                </div>
+
+                                <div class="w-full md:w-1/2 px-2 mb-4">
+                                    <label class="block text-sm">
+                                        Level
+                                    </label>
+                                    <select name="level"  id="level" required class="border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" >
+                                        <option value="0" hidden="">-- Pilih Disini --</option>
+                                        <option value="admin">Admin</option>
+                                        <option value="user">User</option>
+                                    </select>
+                                </div>
+                            </div>
                                 <div class="w-full p-6 space-x-2 border-t border-gray-200 rounded-b dark:border-gray-600">
                                     <div class="flex justify-between">
                                         <button type="submit" class="bg-warning text-white py-2 px-4 rounded shadow-lg flex items-start mx-3">Simpan</button>
